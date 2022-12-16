96c38dc05935b0135ccde533eb32caf929c16a84
Chris <chris@sourcecube.de>
2022-12-16 07:47:38 +0100

    Chore: Remove contentful query container (#15)

--- src/FondOfSpryker/Zed/ContentfulPageSearch/Persistence/ContentfulPageSearchPersistenceFactory.php
+++ src/FondOfSpryker/Zed/ContentfulPageSearch/Persistence/ContentfulPageSearchPersistenceFactory.php
@@ -2,8 +2,6 @@
 
 namespace FondOfSpryker\Zed\ContentfulPageSearch\Persistence;
 
-use FondOfSpryker\Zed\ContentfulPageSearch\ContentfulPageSearchDependencyProvider;
-use FondOfSpryker\Zed\ContentfulPageSearch\Dependency\QueryContainer\ContentfulPageSearchToContentfulQueryContainerInterface;
 use Orm\Zed\ContentfulPageSearch\Persistence\FosContentfulPageSearchQuery;
 use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
 
@@ -18,13 +16,5 @@ class ContentfulPageSearchPersistenceFactory extends AbstractPersistenceFactory
     public function createFobContentfulPageSearchQuery(): FosContentfulPageSearchQuery
     {
         return FosContentfulPageSearchQuery::create();
-    }
-
-    /**
-     * @return \FondOfSpryker\Zed\ContentfulPageSearch\Dependency\QueryContainer\ContentfulPageSearchToContentfulQueryContainerInterface
-     */
-    public function getContentfulEntryQueryContainer(): ContentfulPageSearchToContentfulQueryContainerInterface
-    {
-        return $this->getProvidedDependency(ContentfulPageSearchDependencyProvider::QUERY_CONTAINER_CONTENTFUL_ENTRY);
     }
 }
